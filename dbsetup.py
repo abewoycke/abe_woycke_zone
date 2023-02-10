9e269d3278969959a382b78e1d43ad5da6c9078a
abewoycke <abewoycke@gmail.com>
2023-02-10 17:21:13 -0500

    add sql for new (non-tutorial) db setup

--- dbsetup.py
+++ dbsetup.py
@@ -14,9 +14,31 @@ def create_connection(database):
 
 def create_table(c):
     sql = """
-            CREATE TABLE IF NOT EXISTS poll_options (
-                name varchar(225) PRIMARY KEY,
-                votes integer Default 0
+            CREATE TABLE IF NOT EXISTS dim_participant (
+                participant_pk INTEGER PRIMARY KEY,
+                submitted_name TEXT NOT NULL
+            )
+            CREATE TABLE IF NOT EXISTS dim_poll (
+                poll_pk INTEGER PRIMARY KEY,
+                month INTEGER NOT NULL,
+                year INTEGER NOT NULL,
+                squad TEXT NOT NULL
+            )
+            CREATE TABLE IF NOT EXISTS fact_response (
+                response_pk INTEGER PRIMARY KEY,
+                participant_fk INTEGER NOT NULL,
+                poll_fk INTEGER NOT NULL,
+                submitted_dt INTEGER NOT NULL,
+                poll_option TEXT NOT NULL,
+                response INTEGER DEFAULT 0,
+                FOREIGN KEY (participant_fk)
+                    REFERENCES dim_participant (participant_pk)
+                        ON DELETE CASCADE
+                        ON UPDATE NO ACTION
+                FOREIGN KEY (poll_fk)
+                    REFERENCES dim_poll (poll_pk)
+                        ON DELETE CASCADE
+                        ON UPDATE NO ACTION
             );
         """
     c.execute(sql)
